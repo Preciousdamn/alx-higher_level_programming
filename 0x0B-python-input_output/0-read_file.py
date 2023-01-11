87d0b5db263b1d99f0ad4b640efd5292b60d12b5
Preciousdamn <preciousikiki@gmail.com>
2023-01-11 02:21:39 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,10 +6,3 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-
